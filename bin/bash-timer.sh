69a778fe8d690972458d2559915a623ce4e3d567
dzv <dzv@foyer.lu>
2022-09-01 13:31:25 +0200

    Add fancy prompt

--- bin/bash-timer.sh
+++ bin/bash-timer.sh
@@ -2,12 +2,21 @@
 
 # we need bash >=4.4.
 
+declare BASHTIMER_TTIME
 declare BASHTIMER_TIME
 declare BASHTIMER_LAST_CMD
 
+declare -A BASHTIMER_DEFAULT_COLOR=(
+    [ok]="\033[1;32m"
+    [err]="\033[0;31m"
+    [reset]="\033[0m"
+)
+declare -A BASHTIMER_COLOR
+
 # save time in a temporary file and remove it once it's not used anymore
 BashTimer::setTime(){
-    printf -v BASHTIMER_TIME '%(%s)T'
+    printf -v BASHTIMER_TIME '%(%H:%M:%S)T'
+    printf -v BASHTIMER_TTIME '%(%s)T'
 }
 
 # we could convert function to arithmetic
@@ -35,7 +44,7 @@ BashTimer::PS1(){
 
     printf -v _t '%(%s)T'
 
-    ((timetaken=_t - BASHTIMER_TIME))
+    ((timetaken=_t - BASHTIMER_TTIME))
         
     BashTimer::Convert "$timetaken"
 
@@ -52,12 +61,38 @@ BashTimer::Prompt(){
     }
     printf -v _t '%(%s)T'
 
-    ((timetaken=_t - BASHTIMER_TIME))
+    ((timetaken=_t - BASHTIMER_TTIME))
 
     printf 'Time : %s \n' "$(BashTimer::Convert "$timetaken")"
 
     BashTimer::setTime
     BASHTIMER_LAST_CMD=""
+}
+
+BashTimer::FancyPrompt(){
+    local _t timetaken _n
+    [[ -z "$BASHTIMER_LAST_CMD" ]] && {
+        BashTimer::setTime
+        return
+    }
+    printf -v _t '%(%s)T'
+    printf -v _n '%(%H:%M:%S)T'
+
+    ((timetaken=_t - BASHTIMER_TTIME))
+
+    if (( timetaken < 3 )); then
+        col="${BASHTIMER_COLOR[ok]:-${BASHTIMER_DEFAULT_COLOR[ok]}}"
+    else
+        col="${BASHTIMER_COLOR[err]:-${BASHTIMER_DEFAULT_COLOR[err]}}"
+    fi
+
+    out="[ Taken : $(BashTimer::Convert "$timetaken") | Start Time : $BASHTIMER_TIME , End Time : $_n ]"
+
+    printf "%b%$((COLUMNS - ${#out} + 20))s%b\n" "$col" "$out" "${BASHTIMER_COLOR[reset]:-${BASHTIMER_DEFAULT_COLOR[reset]}}"
+
+    BashTimer::setTime
+    BASHTIMER_LAST_CMD=""
+
 }
 
 BashTimer::Reset(){
